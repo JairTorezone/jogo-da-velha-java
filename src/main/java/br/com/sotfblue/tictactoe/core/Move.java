d85d898c05b33c641cee3c8635dcb417670340f9
Jair Torezone <jairtorezone@gmail.com>
2022-01-09 19:11:51 -0300

    criado construtores das classes, board, move e player

--- src/main/java/br/com/sotfblue/tictactoe/core/Move.java
+++ src/main/java/br/com/sotfblue/tictactoe/core/Move.java
@@ -5,18 +5,23 @@ public class Move {
 	private int i;
 	private int j;
 	
+	public Move(String moverStr) {
+		String[] filds = moverStr.split(",");
+		
+		this.i = Integer.parseInt(filds[0]);
+		this.j = Integer.parseInt(filds[1]);
+		
+		//TODO: validar se a estrutura do moveStr 
+	}
+	
+	//GETTS
 	public int getI() {
 		return i;
 	}
-	public void setI(int i) {
-		this.i = i;
-	}
+
 	public int getJ() {
 		return j;
 	}
-	public void setJ(int j) {
-		this.j = j;
-	}
-	
+
 	
 }
