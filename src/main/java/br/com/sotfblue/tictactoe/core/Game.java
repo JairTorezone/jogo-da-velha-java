377e1720e296e689f73613014e0ae1ac4453e291
Jair Torezone <jairtorezone@gmail.com>
2022-01-17 21:57:03 -0300

    created the players

--- src/main/java/br/com/sotfblue/tictactoe/core/Game.java
+++ src/main/java/br/com/sotfblue/tictactoe/core/Game.java
@@ -13,6 +13,20 @@ public class Game {
 
 		board.print();
 		
-		//UI.readInput("Nome do Jogador:");
+		for (int i = 0; i < players.length; i++) {
+			players[i] = createPlayer(i);
+			System.out.println();
+		}
 	}
+	
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + " =>");
+		char symbol = Constants.SYMBOL_PLAYERS[index];
+		Player player = new Player(name, board, symbol);
+		 
+		UI.printText("O jogador '" + name + " ' vai usar o símbolo '" + symbol + "'");
+		return player;
+	}
+	
+	
 }
